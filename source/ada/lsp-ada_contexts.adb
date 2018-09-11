5b4d2930208caaf8be006fca7ade0de9929f2fd3
Maxim Reznik <reznik@adacore.com>
2018-09-11 19:27:07 +0300

    Use libgpr2 to collect all project sources

--- source/ada/lsp-ada_contexts.adb
+++ source/ada/lsp-ada_contexts.adb
@@ -15,6 +15,15 @@
 -- of the license.                                                          --
 ------------------------------------------------------------------------------
 
+with Ada.Strings.UTF_Encoding.Wide_Wide_Strings;
+
+with GPR2.Path_Name;
+with GPR2.Project.Source.Set;
+pragma Unreferenced (GPR2.Project.Source.Set);
+
+with GPR2.Source;
+with Ada.Wide_Wide_Characters.Unicode;
+
 package body LSP.Ada_Contexts is
 
    ------------------
@@ -35,8 +44,57 @@ package body LSP.Ada_Contexts is
 
    not overriding procedure Initialize
      (Self : in out Context;
-      Root : LSP.Types.LSP_String) is
+      Root : LSP.Types.LSP_String)
+   is
+      function To_Unit_Name (Name : GPR2.Name_Type) return Wide_Wide_String;
+      function To_URI
+        (Path : GPR2.Path_Name.Full_Name) return LSP.Messages.DocumentUri;
+
+      function To_Unit_Name (Name : GPR2.Name_Type) return Wide_Wide_String is
+         Result : Wide_Wide_String :=
+           Ada.Strings.UTF_Encoding.Wide_Wide_Strings.Decode
+             (Ada.Strings.UTF_Encoding.UTF_8_String (Name));
+      begin
+         for X of Result loop
+            X := Ada.Wide_Wide_Characters.Unicode.To_Lower_Case (X);
+         end loop;
+
+         return Result;
+      end To_Unit_Name;
+
+      function To_URI
+        (Path : GPR2.Path_Name.Full_Name) return LSP.Messages.DocumentUri is
+      begin
+         return LSP.Types.To_LSP_String ("file://" & Path);
+      end To_URI;
+
+      Dir  : constant GPR2.Name_Type :=
+        GPR2.Name_Type (LSP.Types.To_UTF_8_String (Root));
+
+      Path : constant GPR2.Path_Name.Object :=
+        GPR2.Path_Name.Create_File
+          (Name      => "gnat/lsp.gpr",
+           Directory => Dir);
    begin
+      Self.Project_Tree.Load
+        (Filename => Path,
+         Context  => Self.GPR_Context);
+
+      for Source of Self.Project_Tree.Root_Project.Sources loop
+         declare
+            Object : constant GPR2.Source.Object := Source.Source;
+            Name   : constant GPR2.Name_Type := Object.Unit_Name;
+            Kind   : constant GPR2.Source.Kind_Type := Object.Kind;
+            File   : constant GPR2.Path_Name.Object := Object.Path_Name;
+         begin
+            if Kind in GPR2.Source.S_Spec then
+               Self.Specs.Insert (To_Unit_Name (Name), To_URI (File.Value));
+            else
+               Self.Bodies.Insert (To_Unit_Name (Name), To_URI (File.Value));
+            end if;
+         end;
+      end loop;
+
       Self.Root := Root;
    end Initialize;
 
--- source/ada/lsp-ada_contexts.ads
+++ source/ada/lsp-ada_contexts.ads
@@ -17,6 +17,11 @@
 
 with Ada.Containers.Hashed_Maps;
 
+private with Ada.Containers.Indefinite_Hashed_Maps;
+private with Ada.Strings.Wide_Wide_Hash;
+private with GPR2.Context;
+private with GPR2.Project.Tree;
+
 with LSP.Messages;
 
 with LSP.Ada_Documents;
@@ -52,9 +57,21 @@ private
       Equivalent_Keys => LSP.Types."=",
       "="             => LSP.Ada_Documents."=");
 
+   package Unit_Maps is new Ada.Containers.Indefinite_Hashed_Maps
+     (Key_Type        => Wide_Wide_String,
+      Element_Type    => LSP.Messages.DocumentUri,
+      Hash            => Ada.Strings.Wide_Wide_Hash,
+      Equivalent_Keys => "=",
+      "="             => LSP.Types."=");
+   --  Map from LAL unit name to LSP document uri
+
    type Context is tagged limited record
-      Root        : LSP.Types.LSP_String;
-      Documents   : Document_Maps.Map;
+      GPR_Context  : GPR2.Context.Object;
+      Project_Tree : GPR2.Project.Tree.Object;
+      Root         : LSP.Types.LSP_String;
+      Specs        : Unit_Maps.Map;
+      Bodies       : Unit_Maps.Map;
+      Documents    : Document_Maps.Map;
    end record;
 
 end LSP.Ada_Contexts;
