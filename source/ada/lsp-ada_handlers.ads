88bdb784c92d4d94f8f40ca7c4a4171f17e9f369
raja-s <rjsoufi@gmail.com>
2018-10-29 13:07:53 -0400

    Reordered handler definitions again to maintain lexical order

--- source/ada/lsp-ada_handlers.ads
+++ source/ada/lsp-ada_handlers.ads
@@ -39,10 +39,25 @@ private
       null;
    end record;
 
+   overriding procedure Exit_Notification
+     (Self : access Message_Handler);
+
    overriding procedure Initialize_Request
      (Self     : access Message_Handler;
       Value    : LSP.Messages.InitializeParams;
       Response : in out LSP.Messages.Initialize_Response);
+
+   overriding procedure Text_Document_Did_Change
+     (Self  : access Message_Handler;
+      Value : LSP.Messages.DidChangeTextDocumentParams);
+
+   overriding procedure Text_Document_Did_Close
+     (Self  : access Message_Handler;
+      Value : LSP.Messages.DidCloseTextDocumentParams);
+
+   overriding procedure Text_Document_Did_Open
+     (Self  : access Message_Handler;
+      Value : LSP.Messages.DidOpenTextDocumentParams);
 
    overriding procedure Text_Document_References_Request
      (Self     : access Message_Handler;
@@ -53,20 +68,5 @@ private
     (Self     : access Message_Handler;
      Value    : LSP.Messages.DocumentSymbolParams;
      Response : in out LSP.Messages.Symbol_Response);
-
-   overriding procedure Text_Document_Did_Open
-     (Self  : access Message_Handler;
-      Value : LSP.Messages.DidOpenTextDocumentParams);
-
-   overriding procedure Text_Document_Did_Change
-     (Self  : access Message_Handler;
-      Value : LSP.Messages.DidChangeTextDocumentParams);
-
-   overriding procedure Text_Document_Did_Close
-     (Self  : access Message_Handler;
-      Value : LSP.Messages.DidCloseTextDocumentParams);
-
-   overriding procedure Exit_Notification
-     (Self : access Message_Handler);
 
 end LSP.Ada_Handlers;
