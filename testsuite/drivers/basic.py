46bf184cb5667da685fcccd66b1ec57e83f5c46e
Nicolas Setton <setton@adacore.com>
2019-02-26 10:51:52 -0500

    Add an e3-based testsuite driver for the ALS
    For integration in the AdaCore testing system.

---
+++ testsuite/drivers/basic.py
@@ -0,0 +1,82 @@
+from e3.fs import mkdir, sync_tree, echo_to_file
+from e3.testsuite.process import Run
+from e3.testsuite.result import TestStatus
+from drivers import ALSTestDriver
+import os
+import glob
+
+
+class JsonTestDriver(ALSTestDriver):
+    """ Each test should have:
+          - a test.yaml containing
+                title: '<test name>'
+
+          - a number of test drivers, in .json files.
+    """
+
+    def add_test(self, dag):
+        self.add_fragment(dag, 'prepare')
+        self.add_fragment(dag, 'run', after=['prepare'])
+
+    def prepare(self, previous_values):
+        mkdir(self.test_env['working_dir'])
+        sync_tree(self.test_env['test_dir'],
+                  self.test_env['working_dir'])
+
+    def run(self, previous_values):
+        # Check whether the test should be skipped
+        skip = self.should_skip()
+        if skip is not None:
+            self.result.set_status(skip)
+            self.push_result()
+            return False
+
+        # The working directory
+        wd = self.test_env['working_dir']
+
+        # The base directory for the repository
+        base = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                            "..", ".."))
+
+        # Where the als resides
+        als = os.path.join(base, '.obj', 'server', 'ada_language_server')
+
+        # Where the test driver resides
+        tester = os.path.join(base, '.obj', 'tester', 'tester-run')
+
+        output = ""
+
+        status = TestStatus.PASS
+
+        for json in glob.glob(os.path.join(wd, '*.json')):
+            process = Run(
+                [tester, json],
+                cwd=wd,
+                timeout=120,
+                env={'ALS': als},
+                ignore_environ=False)
+            output += process.out
+
+            if process.status:
+                # Nonzero status?
+                if process.status == 100:
+                    # This one is an xfail
+                    status = TestStatus.XFAIL
+                    break
+                else:
+                    # Unknown status!
+                    status = TestStatus.ERROR
+                    break
+            else:
+                # Status is 0...
+                if output:
+                    # ... and there is an output: that's a FAIL
+                    status = TestStatus.FAIL
+                    break
+
+        self.result.set_status(status)
+        if output:
+            # If there's an output, capture it
+            self.result.out = output
+
+        self.push_result()
---
+++ testsuite/testsuite.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from drivers.basic import JsonTestDriver
+from e3.fs import ls
+from e3.testsuite import Testsuite
+from e3.os.process import Run, STDOUT
+from e3.env import Env
+import os
+import logging
+import tempfile
+
+
+class ALSTestsuite(Testsuite):
+    TEST_SUBDIR = 'ada_lsp'
+    DRIVERS = {'default': JsonTestDriver}
+
+    def add_options(self):
+        self.main.argument_parser.add_argument(
+            "--build",
+            default="",
+            action="store",
+            help="Ignored, here for compatibility purposes")
+
+    def tear_up(self):
+        pass
+
+    def tear_down(self):
+        super(ALSTestsuite, self).tear_down()
+
+    def get_test_list(self, sublist):
+        # The tests are one per subdir of "ada_lsp"
+        if sublist:
+            dirs = [os.path.abspath(os.path.join(self.test_dir, '..', s))
+                    for s in sublist]
+        else:
+            dirs = ls(os.path.join(self.test_dir, '*'))
+        results = []
+        for d in dirs:
+            if os.path.isdir(d):
+                # Create the test.yamls if they don't exist!
+                yaml = os.path.join(d, 'test.yaml')
+                basename = os.path.basename(d)
+
+                if not os.path.exists(yaml):
+                    with open(yaml, 'wb') as f:
+                        logging.info("creating {} for you :-)".format(yaml))
+                        f.write("title: '{}'\n".format(basename))
+                results.append(os.path.join(basename, 'test.yaml'))
+
+        logging.info('Found %s tests %s', len(results), results)
+        logging.debug("tests:\n  " + "\n  ".join(results))
+        return results
+
+    @property
+    def default_driver(self):
+        return 'default'
