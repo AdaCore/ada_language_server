4ecd88b8996fa57b7a5e4018b8cc9d3f29ff8033
Nicolas Setton <setton@adacore.com>
2019-05-03 16:02:25 -0400

    Add new utility packages.

---
+++ source/protocol/lsp-messages-common_writers.ads
@@ -0,0 +1,43 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                       Copyright (C) 2019, AdaCore                        --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+--  This package provides utilities for factorizing the coding/decoding
+--  of common message types. It is meant to be used internally only.
+
+package LSP.Messages.Common_Writers is
+
+   procedure Write_Notification_Prefix
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : LSP.Messages.NotificationMessage'Class);
+   --  Write the data common to all notifications in V
+
+   procedure Write_Request_Prefix
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : LSP.Messages.RequestMessage'Class);
+   --  Write the data common to all requests in V
+
+   procedure Set_Common_Notification_Fields
+     (R  : in out NotificationMessage'Class;
+      JS : in out LSP.JSON_Streams.JSON_Stream'Class);
+   --  Read the data common to all notifications in R
+
+   procedure Set_Common_Request_Fields
+     (R  : in out RequestMessage'Class;
+      JS : in out LSP.JSON_Streams.JSON_Stream'Class);
+   --  Read the data common to all requests in R
+
+end LSP.Messages.Common_Writers;
