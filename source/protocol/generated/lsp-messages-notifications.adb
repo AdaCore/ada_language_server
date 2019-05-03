084dfde95785d4a2aee34e79667349335c20e503
Nicolas Setton <setton@adacore.com>
2019-05-03 15:55:40 -0400

    Add automatic generation of Notifications

---
+++ source/protocol/generated/lsp-messages-notifications.adb
@@ -0,0 +1,410 @@
+--  Automatically generated, do not edit.
+
+with Ada.Strings.UTF_Encoding;
+with LSP.JSON_Streams;
+with LSP.Types; use LSP.Types;
+with LSP.Messages.Common_Writers; use LSP.Messages.Common_Writers;
+
+package body LSP.Messages.Notifications is
+
+   function "+" (Text : Ada.Strings.UTF_Encoding.UTF_8_String)
+      return LSP.Types.LSP_String renames
+       LSP.Types.To_LSP_String;
+
+   function Decode_Notification
+      (Document : JSON_Value) return NotificationMessage'Class
+   is
+      JS : aliased LSP.JSON_Streams.JSON_Stream;
+      JSON_Array : GNATCOLL.JSON.JSON_Array;
+
+      Method     : LSP.Types.LSP_String;
+
+   begin
+      GNATCOLL.JSON.Append (JSON_Array, Document);
+      JS.Set_JSON_Document (JSON_Array);
+      JS.Start_Object;
+
+      LSP.Types.Read_String (JS, +"method", Method);
+
+      if To_UTF_8_String (Method) = "initialized" then
+         declare
+            R : Initialized_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "exit" then
+         declare
+            R : Exit_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "workspace/didChangeConfiguration" then
+         declare
+            R : DidChangeConfiguration_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            DidChangeConfigurationParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "window/showMessage" then
+         declare
+            R : ShowMessage_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            ShowMessageParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "window/logMessage" then
+         declare
+            R : LogMessage_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            LogMessageParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "textDocument/publishDiagnostics" then
+         declare
+            R : PublishDiagnostics_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            PublishDiagnosticsParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "textDocument/didOpen" then
+         declare
+            R : DidOpenTextDocument_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            DidOpenTextDocumentParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "textDocument/didChange" then
+         declare
+            R : DidChangeTextDocument_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            DidChangeTextDocumentParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "textDocument/didSave" then
+         declare
+            R : DidSaveTextDocument_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            DidSaveTextDocumentParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      if To_UTF_8_String (Method) = "textDocument/didClose" then
+         declare
+            R : DidCloseTextDocument_Notification;
+         begin
+            Set_Common_Notification_Fields (R, JS);
+            JS.Key ("params");
+            DidCloseTextDocumentParams'Read (JS'Access, R.params);
+            return R;
+         end;
+      end if;
+
+      raise Program_Error; --  Notification not found
+   end Decode_Notification;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Initialized_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : Initialized_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Exit_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : Exit_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out DidChangeConfiguration_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      DidChangeConfigurationParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : DidChangeConfiguration_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      DidChangeConfigurationParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out ShowMessage_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      ShowMessageParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : ShowMessage_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      ShowMessageParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out LogMessage_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      LogMessageParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : LogMessage_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      LogMessageParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out PublishDiagnostics_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      PublishDiagnosticsParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : PublishDiagnostics_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      PublishDiagnosticsParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out DidOpenTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      DidOpenTextDocumentParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : DidOpenTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      DidOpenTextDocumentParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out DidChangeTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      DidChangeTextDocumentParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : DidChangeTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      DidChangeTextDocumentParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out DidSaveTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      DidSaveTextDocumentParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : DidSaveTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      DidSaveTextDocumentParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out DidCloseTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Set_Common_Notification_Fields (V, JS);
+      JS.Key ("params");
+      DidCloseTextDocumentParams'Read (S, V.params);
+      JS.End_Object;
+   end Read;
+
+   procedure Write
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : DidCloseTextDocument_Notification)
+   is
+      JS : LSP.JSON_Streams.JSON_Stream'Class renames
+        LSP.JSON_Streams.JSON_Stream'Class (S.all);
+   begin
+      JS.Start_Object;
+      Write_Notification_Prefix (S, V);
+      JS.Key ("params");
+      DidCloseTextDocumentParams'Write (S, V.params);
+      JS.End_Object;
+   end Write;
+
+end LSP.Messages.Notifications;
--- source/protocol/generated/lsp-messages-requests.ads
+++ source/protocol/generated/lsp-messages-requests.ads
@@ -5,128 +5,203 @@ with GNATCOLL.JSON; use GNATCOLL.JSON;
 
 package LSP.Messages.Requests is
 
-   function Decode_Request (Document : JSON_Value) return RequestMessage'Class;
+   function Decode_Request
+     (Document : JSON_Value) return RequestMessage'Class;
    --  Decode the request present in the input document. Document is a JSON
    --  representation of the protocol string.
 
-   type Initialize_Request is new RequestMessage with record
+   type Initialize_Request is new RequestMessage with
+   record
       params : InitializeParams;
    end record;
 
    type Shutdown_Request is new RequestMessage with null record;
 
-   type ShowMessage_Request is new RequestMessage with record
+   type ShowMessage_Request is new RequestMessage with
+   record
       params : ShowMessageRequestParams;
    end record;
 
-   type CodeAction_Request is new RequestMessage with record
+   type CodeAction_Request is new RequestMessage with
+   record
       params : CodeActionParams;
    end record;
 
-   type Completion_Request is new RequestMessage with record
+   type Completion_Request is new RequestMessage with
+   record
       params : TextDocumentPositionParams;
    end record;
 
-   type Definition_Request is new RequestMessage with record
+   type Definition_Request is new RequestMessage with
+   record
       params : TextDocumentPositionParams;
    end record;
 
-   type Highlight_Request is new RequestMessage with record
+   type Highlight_Request is new RequestMessage with
+   record
       params : TextDocumentPositionParams;
    end record;
 
-   type Hover_Request is new RequestMessage with record
+   type Hover_Request is new RequestMessage with
+   record
       params : TextDocumentPositionParams;
    end record;
 
-   type References_Request is new RequestMessage with record
+   type References_Request is new RequestMessage with
+   record
       params : ReferenceParams;
    end record;
 
-   type Signature_Help_Request is new RequestMessage with record
+   type Signature_Help_Request is new RequestMessage with
+   record
       params : TextDocumentPositionParams;
    end record;
 
-   type Document_Symbols_Request is new RequestMessage with record
+   type Document_Symbols_Request is new RequestMessage with
+   record
       params : DocumentSymbolParams;
    end record;
 
-   type Execute_Command_Request is new RequestMessage with record
+   type Execute_Command_Request is new RequestMessage with
+   record
       params : ExecuteCommandParams;
    end record;
 
-   type ApplyWorkspaceEdit_Request is new RequestMessage with record
+   type ApplyWorkspaceEdit_Request is new RequestMessage with
+   record
       params : ApplyWorkspaceEditParams;
    end record;
 
-   type Workspace_Symbols_Request is new RequestMessage with record
+   type Workspace_Symbols_Request is new RequestMessage with
+   record
       params : WorkspaceSymbolParams;
    end record;
 
-   type Workspace_Execute_Command_Request is new RequestMessage with record
+   type Workspace_Execute_Command_Request is new RequestMessage with
+   record
       params : ExecuteCommandParams;
    end record;
 
 private
 
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Initialize_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Initialize_Request);
+   for Initialize_Request'Read use Read;
    for Initialize_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Shutdown_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Shutdown_Request);
+   for Shutdown_Request'Read use Read;
    for Shutdown_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out ShowMessage_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : ShowMessage_Request);
+   for ShowMessage_Request'Read use Read;
    for ShowMessage_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out CodeAction_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : CodeAction_Request);
+   for CodeAction_Request'Read use Read;
    for CodeAction_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Completion_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Completion_Request);
+   for Completion_Request'Read use Read;
    for Completion_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Definition_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Definition_Request);
+   for Definition_Request'Read use Read;
    for Definition_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Highlight_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Highlight_Request);
+   for Highlight_Request'Read use Read;
    for Highlight_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Hover_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Hover_Request);
+   for Hover_Request'Read use Read;
    for Hover_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out References_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : References_Request);
+   for References_Request'Read use Read;
    for References_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Signature_Help_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Signature_Help_Request);
+   for Signature_Help_Request'Read use Read;
    for Signature_Help_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Document_Symbols_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Document_Symbols_Request);
+   for Document_Symbols_Request'Read use Read;
    for Document_Symbols_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Execute_Command_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Execute_Command_Request);
+   for Execute_Command_Request'Read use Read;
    for Execute_Command_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out ApplyWorkspaceEdit_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : ApplyWorkspaceEdit_Request);
+   for ApplyWorkspaceEdit_Request'Read use Read;
    for ApplyWorkspaceEdit_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Workspace_Symbols_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Workspace_Symbols_Request);
+   for Workspace_Symbols_Request'Read use Read;
    for Workspace_Symbols_Request'Write use Write;
+   procedure Read
+     (S : access Ada.Streams.Root_Stream_Type'Class;
+      V : out Workspace_Execute_Command_Request);
    procedure Write
      (S : access Ada.Streams.Root_Stream_Type'Class;
       V : Workspace_Execute_Command_Request);
+   for Workspace_Execute_Command_Request'Read use Read;
    for Workspace_Execute_Command_Request'Write use Write;
 end LSP.Messages.Requests;
