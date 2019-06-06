286f2d6bb14ccd4583034b26fd923e1b80cf522c
Maxim Reznik <reznik@adacore.com>
2019-06-06 17:26:33 +0300

    Fix deploy on Windows after upgrading GNAT to 2019

--- integration/appveyor/deploy.sh
+++ integration/appveyor/deploy.sh
@@ -13,7 +13,7 @@ function add_dll()
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
   <assemblyIdentity
      name="$1"
-     version="1.0.2018.0523"
+     version="1.0.2019.0523"
      processorArchitecture="amd64" type="win32" />
   <file name="$1.dll" />
 </assembly>
@@ -24,7 +24,7 @@ EOF
   <dependentAssembly>
     <assemblyIdentity
        type="win32" name="$1"
-       version="1.0.2018.0523"
+       version="1.0.2019.0523"
        processorArchitecture="amd64"
        language="*" />
   </dependentAssembly>
@@ -36,7 +36,7 @@ cat > $DIR/ada_language_server.exe.manifest <<EOF
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1"
   manifestVersion="1.0">
-<assemblyIdentity version="1.0.2018.0523"
+<assemblyIdentity version="1.0.2019.0523"
   processorArchitecture="amd64"
   name="ada_language_server"
   type="win32"/>
@@ -53,8 +53,8 @@ add_dll libxmlada_sax           $GNAT/bin/
 add_dll libxmlada_schema        $GNAT/bin/
 add_dll libxmlada_unicode       $GNAT/bin/
 add_dll libgcc_s_seh-1          $GNAT/bin/
-add_dll libgnarl-2018           $GNAT/bin/
-add_dll libgnat-2018            $GNAT/bin/
+add_dll libgnarl-2019           $GNAT/bin/
+add_dll libgnat-2019            $GNAT/bin/
 add_dll libgpr                  $GNAT/bin/
 add_dll libgmp-10               /mingw64/bin/
 add_dll libiconv-2              /mingw64/bin/
