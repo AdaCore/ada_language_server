57c34d67b9ecdbe06b38f194ae46da3bf322e772
Maxim Reznik <reznik@adacore.com>
2019-08-02 19:47:39 +0300

    S516-012 Add Server_Request common ancestor
    with Visit procedure. Inherit all server requests from this type.
    Add Server_Request_Reciever type to handle all server requests.
    
    To the same for client requests.
    
    Move Server_Request_Handler to LSP_Server project.

---
+++ source/protocol/generated/lsp-messages-server_requests.adb
@@ -0,0 +1,119 @@
+--  Automatically generated, do not edit.
+
+package body LSP.Messages.Server_Requests is
+
+   --  These messages are sent from client to server.
+
+   overriding procedure Visit
+     (Self    : Initialize_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Initialize_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Shutdown_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Shutdown_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : CodeAction_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_CodeAction_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Completion_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Completion_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Definition_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Definition_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Type_Definition_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Type_Definition_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Highlight_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Highlight_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Hover_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Hover_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : References_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_References_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Signature_Help_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Signature_Help_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Document_Symbols_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Document_Symbols_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Rename_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Rename_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Execute_Command_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Execute_Command_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Workspace_Symbols_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Workspace_Symbols_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : Workspace_Execute_Command_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_Workspace_Execute_Command_Request (Self);
+   end Visit;
+
+   overriding procedure Visit
+     (Self    : ALS_Called_By_Request;
+      Handler : access Server_Request_Receiver'Class) is
+   begin
+      Handler.On_ALS_Called_By_Request (Self);
+   end Visit;
+
+end LSP.Messages.Server_Requests;
