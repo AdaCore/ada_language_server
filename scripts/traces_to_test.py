808a6b48a694feb039890717a018b1813af2d563
Nicolas Setton <setton@adacore.com>
2019-08-19 10:53:27 -0400

    Allow passing the traces traces_to_test.py
    Minor enhancement to ease the creation of tests from GPS logs.

--- scripts/traces_to_test.py
+++ scripts/traces_to_test.py
@@ -5,7 +5,7 @@
 
 To use this, do
 
-   traces_to_test.py    path_to_project_root    > name_of_test_driver.json
+   traces_to_test.py   path_to_project_root  [path_to_traces_file]  > name_of_test_driver.json
 
 """
 
@@ -14,8 +14,13 @@ import os
 import json
 from json_transformations import python_to_protocol_string, traces_to_test
 
-als_dir = os.path.join(os.path.expanduser('~'), '.als')
-inout_file = os.path.join(als_dir, 'inout.txt')
 root = os.path.abspath(sys.argv[1])
+
+if len(sys.argv) > 2:
+    inout_file = sys.argv[2]
+else:
+    als_dir = os.path.join(os.path.expanduser('~'), '.als')
+    inout_file = os.path.join(als_dir, 'inout.txt')
+
 test = traces_to_test(inout_file, root)
 print json.dumps(test, indent=3)
