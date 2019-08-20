4716be503ebe2e48a6f422c1d00d6b51069dd65a
Anthony Leonardo Gracio <leonardo@adacore.com>
2019-08-20 12:19:09 +0200

    Import F222-029.xrefs.prefixed_notation to ALS testsuite

---
+++ testsuite/ada_lsp/F222-029.xrefs.prefixed_notation/prefixed_notation_test.adb
@@ -0,0 +1,8 @@
+with Test_Class; use Test_Class;
+
+procedure Prefixed_Notation_Test is
+   A : My_Tagged_Type;
+begin
+   A.Do_Something;
+end Prefixed_Notation_Test;
+
---
+++ testsuite/ada_lsp/F222-029.xrefs.prefixed_notation/test_class.ads
@@ -0,0 +1,7 @@
+package Test_Class is
+
+   type My_Tagged_Type is tagged null record;
+   
+   procedure Do_Something (This : My_Tagged_Type) is null;
+
+end Test_Class;
