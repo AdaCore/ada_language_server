a32accf4c07999997f9c6c407e96168c7270207d
Nicolas Setton <setton@gmail.com>
2019-09-09 14:29:01 +0000

    S820-016 Add test

---
+++ testsuite/ada_lsp/S820-016.called_by.entry/phil.ads
@@ -0,0 +1,6 @@
+package phil is
+   task type philosopher is
+      entry Start_Eating (Chopstick1 : in Positive;
+                        Chopstick2 : in Positive);
+   end philosopher;
+end Phil;
---
+++ testsuite/ada_lsp/S820-016.called_by.entry/room.adb
@@ -0,0 +1,12 @@
+with phil;
+
+procedure room is
+
+   t : phil.philosopher;
+
+begin
+
+   t.Start_Eating (1, 2);
+   t.Start_Eating (3, 4);
+
+end;
