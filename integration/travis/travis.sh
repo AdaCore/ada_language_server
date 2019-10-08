4d8a21abd42111f86826795a4aa213f54efd68fb
Maxim Reznik <reznik@adacore.com>
2019-10-08 18:58:57 +0300

    SA07-038 Simplification of docker scripts

--- integration/travis/travis.sh
+++ integration/travis/travis.sh
@@ -2,17 +2,13 @@
 
 function linux_before_install()
 {
-    cp -R integration/travis /tmp/
-    cd ..
-    tar --exclude=.git \
-        -c -z -f /tmp/travis/lsp.tar.gz ada_language_server
-    docker build --tag lsp /tmp/travis
+    docker build --tag lsp --file integration/travis/Dockerfile .
 }
 
 function linux_script()
 {
     docker run -i -t -v$(pwd)/upload:/upload lsp /bin/bash -c \
- 'tar xzf /tmp/lsp.tar.gz && make -C ada_language_server LIBRARY_TYPE=relocatable deploy'
+ 'make -C /tmp/ada_language_server LIBRARY_TYPE=relocatable deploy'
 
 }
 
