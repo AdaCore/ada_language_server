38d3905e328339342c3fccc121e8a5465657c8cb
Maxim Reznik <reznik@adacore.com>
2019-12-19 19:40:20 +0200

    Get rid of docker while buildingunder Linux.i
    Use cache to speed up gnat installation.

--- integration/travis/travis.sh
+++ integration/travis/travis.sh
@@ -2,41 +2,57 @@
 
 set -e -x
 
+INSTALL_DIR=$PWD/../gnat
+export PATH=$INSTALL_DIR/bin:$PATH
+
+function download_gnat()
+{
+    if [ ! -f $GNAT_INSTALLER ]; then
+        mkdir -p `dirname $GNAT_INSTALLER`
+        # Use --progress=dot:giga to ensure travis doesn't give up for lack of progress
+        wget --progress=dot:giga -O $GNAT_INSTALLER $1
+    fi
+
+    git clone https://github.com/AdaCore/gnat_community_install_script.git
+    sh gnat_community_install_script/install_package.sh $GNAT_INSTALLER $INSTALL_DIR
+    $INSTALL_DIR/bin/gprinstall --uninstall gnatcoll
+}
+
 function linux_before_install()
 {
-    docker build --tag lsp --file integration/travis/Dockerfile .
+    echo INSTALL_DIR=$INSTALL_DIR
+    GNAT_INSTALLER=$HOME/cache/gnat-install
+
+    download_gnat http://mirrors.cdn.adacore.com/art/5cdffc5409dcd015aaf82626
+
+    wget -nv -O- https://dl.bintray.com/reznikmm/libadalang/libadalang-stable-linux.tar.gz \
+        | tar xzf - -C $INSTALL_DIR
+    sudo apt-get update
+    sudo apt-get -y install chrpath
 }
 
 function linux_script()
 {
     TAG=${TRAVIS_TAG:-latest}
     sed -i -e "s/VERSION/$TAG/g" integration/travis/bintray.json
-    docker run -i -t -v$(pwd)/upload:/upload --env TRAVIS_TAG lsp /bin/bash -c \
- 'make -C /tmp/ada_language_server LIBRARY_TYPE=relocatable deploy'
-
+    make LIBRARY_TYPE=relocatable check
+    integration/travis/deploy.sh linux
 }
 
-GNAT_INSTALLER=gnat-community-2019-20190517-x86_64-darwin-bin.dmg
-INSTALL_DIR=$PWD/../gnat
 
 function osx_before_install()
 {
     echo INSTALL_DIR=$INSTALL_DIR
-    git clone https://github.com/AdaCore/gnat_community_install_script.git
+    GNAT_INSTALLER=$HOME/cache/gnat-community-2019-20190517-x86_64-darwin-bin.dmg
 
-    # Use --progress=dot:mega to ensure travis doesn't give up for lack of progress
-    wget --progress=dot:mega -O $GNAT_INSTALLER \
-        http://mirrors.cdn.adacore.com/art/5ce0322c31e87a8f1d4253fa
-    sh gnat_community_install_script/install_package.sh \
-        $GNAT_INSTALLER $INSTALL_DIR
-    $INSTALL_DIR/bin/gprinstall --uninstall gnatcoll
+    download_gnat http://mirrors.cdn.adacore.com/art/5ce0322c31e87a8f1d4253fa
+
     wget -nv -O- https://dl.bintray.com/reznikmm/libadalang/libadalang-stable-osx.tar.gz \
         | tar xzf - -C $INSTALL_DIR
 }
 
 function osx_script()
 {
-    export PATH=$INSTALL_DIR/bin:$PATH
     export LIBRARY_PATH=/usr/local/lib        # To find GMP
     make OS=osx LIBRARY_TYPE=relocatable all
     integration/travis/deploy.sh darwin
