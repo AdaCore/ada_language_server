6262ca324392fc0334c71f28bbc51ef8bf000b25
Maxim Reznik <reznik@adacore.com>
2020-03-11 13:57:01 +0200

    T302-034 Fix named params refactoring
    for cases with absent optional arguments.

--- testsuite/ada_lsp/named_params/test.adb
+++ testsuite/ada_lsp/named_params/test.adb
@@ -1,8 +1,10 @@
 procedure Test is
    type T is mod 2;
    procedure P (Pr : T);
-   procedure B (Br : T; X : Natural) is null;
+   procedure B (Br : T; X : Natural := 0) is null;
 begin
    P (1);
    B (2, 0);
+   P (3);
+   B (2);
 end;
