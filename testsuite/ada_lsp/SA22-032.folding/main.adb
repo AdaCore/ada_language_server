25f1ef6276360673b833e7d1f94add4e6aedb1db
Andry Ogorodnik <ogorodnik@adacore.com>
2020-03-12 18:10:52 +0200

    SA22-032 Corrections for the code folding
    - don't process Ada_Subp_Spec, it is a part of Ada_Subp_Decl
    
    - don't process Ada_Aspect_Assoc_List and Ada_Aspect_Assoc:
        parts of Ada_Aspect_Spec

--- testsuite/ada_lsp/SA22-032.folding/main.adb
+++ testsuite/ada_lsp/SA22-032.folding/main.adb
@@ -7,6 +7,19 @@ procedure Main is
    use Ada.Strings.Unbounded;
 
    S : Unbounded_String := To_Unbounded_String ("Hello");
+
+   --  Block for the procedure should also include conditions [12:0-16:0]
+   procedure A
+     (X : Integer;
+      Y : Integer)
+     with Pre => X > -2
+     and then Y > -2;
+
+   procedure A is
+   begin
+      null;
+   end A;
+
 begin
 
    -- simple comment
