1db3ae11a808f01ee57149d6fbf9369728970cb3
Adrien Boulanger <boulanger@adacore.com>
2020-03-17 17:01:28 +0100

    T317-049: Remove duplicate results in Is_Called_By request
    Duplicates were happening when concatening the results from
    different contexts.
    
    Add a test.

---
+++ testsuite/ada_lsp/aggregate.is_called_by/common/common_pack.adb
@@ -0,0 +1,21 @@
+package body Common_Pack is
+
+   ---------
+   -- Foo --
+   ---------
+
+   procedure Foo is
+   begin
+      Bar;
+   end Foo;
+
+   ---------
+   -- Bar --
+   ---------
+
+   procedure Bar is
+   begin
+      null;
+   end Bar;
+
+end Common_Pack;
