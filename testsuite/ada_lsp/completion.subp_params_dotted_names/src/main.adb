34dc274d3a3659b4e1e46af69a198fc0476cbc60
Anthony Leonardo Gracio <leonardo@adacore.com>
2020-04-16 17:01:21 +0200

    T319-027: Handle package prefixes for subp calls completion
    We now use the new libadalang Is_Dot_Call function on the returned
    compeltion items to know if we are dealing with a dotted subporgram call
    or not, allowing us to distiguish dotted calls, for which we should omit
    the first parameter in the returned completion snippets, from subprogram
    calls that are prefixed by a package names (e.g: Ada.Text_IO.Put_Line).
    
    An automatic test has also been added.

---
+++ testsuite/ada_lsp/completion.subp_params_dotted_names/src/main.adb
@@ -0,0 +1,13 @@
+with Bar; use Bar;
+
+procedure Main is
+   Obj : My_Int := (A => 10);
+
+   function Add (A, B : Integer) return Integer
+   is
+      (A + B);
+
+   A : Integer := 3;
+begin
+   Bar.Do_
+end Main;
