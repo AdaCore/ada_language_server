41041b84225b2429154f9d49a558749f173a0cff
Anthony Leonardo Gracio <leonardo@adacore.com>
2020-05-28 15:55:33 +0200

    SA14-008: handle aggregate completion of derived types.
    When initializing a record that derives from a parent type, the visible
    components/discriminants should be specified too.
    Also, when the parent is private, aggregate extensions should be used in
    the proposed snippets.
    
    An automatic test has been added.

---
+++ testsuite/ada_lsp/completion.aggregates.derived_private/main.adb
@@ -0,0 +1,31 @@
+procedure Main is
+
+   package My_Package is
+
+      type Base_Rec (Disc : Integer) is tagged private;
+
+   private
+
+      type Base_Rec (Disc : Integer) is tagged record
+         case Disc is
+         when 1 =>
+            A : Integer;
+         when 2 =>
+            Z : Integer;
+         when others =>
+            B : Integer;
+         end case;
+      end record;
+   end My_Package;
+
+   use My_Package;
+
+   type Derived_Rec is new Base_Rec with record
+      D : Integer;
+      X : Float;
+   end record;
+
+   Obj : Derived_Rec :=
+   begin
+   null;
+end Main;
