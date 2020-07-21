ada4faa7a686ae020163425df484fb1633f33969
Adrien Boulanger <boulanger@adacore.com>
2020-07-21 13:57:35 +0200

    S312-063: separate variables and constants in documentSymbol
    Add a test.

---
+++ testsuite/ada_lsp/S312-063.documentSymbol.constant/foo.adb
@@ -0,0 +1,11 @@
+procedure Foo is
+   A : constant Character := 'A';
+   B : Character := 'B';
+begin
+   declare
+      C : constant Character := 'C';
+      D : Character := 'D';
+   begin
+      null;
+   end;
+end Foo;
