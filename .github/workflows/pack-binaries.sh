b334cbf5122eca38d81401fc7c26d93774231837
Maxim Reznik <reznik@adacore.com>
2020-12-14 19:06:28 +0200

    TC07-049 Replace CI with GitHub Actions
    and build ALS statically. Build ALS in dev and prod modes.
    Strip debug information in the prod mode before package
    the *.vsix.

---
+++ .github/workflows/pack-binaries.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x -e
+DEBUG=$1 # Value is '' or 'debug'
+VSCE_TOKEN=$2
+TAG=$3 # For master it's 22.0.999, while for tag it's the tag itself
+
+function make_change_log()
+{
+    echo "# Release notes"
+    echo ""
+    for TAG_ID in `git tag --list '2*' | tac` ; do
+        DATE=`git show --no-patch --format=Date:%ad --date=short $TAG_ID |\
+          grep Date: | sed -e s/Date://`
+        echo "## $TAG_ID ($DATE)"
+        git show --no-patch --format=%n $TAG_ID | sed -e '1,/Release notes/d'
+    done
+}
+
+chmod -R -v +x als-*-$DEBUG
+for X in Linux macOS Windows ; do mv -v -f als-$X-$DEBUG/* integration/vscode/ada/; done
+cp -v -f LICENSE integration/vscode/ada/
+pushd integration/vscode/ada
+sed -i -e "/version/s/[0-9][0-9.]*/$TAG/" package.json
+[ -z "$DEBUG" ] || sed -i -e '/^    "name"/s/ada/ada-debug/' package.json
+npm install
+sudo npm install -g vsce
+make_change_log > CHANGELOG.md
+[[ ${GITHUB_REF##*/} != 2*.[0-9]*.[0-9]* ]] || vsce publish -p "$VSCE_TOKEN" || true
+vsce package || true
+popd
+mv -v integration/vscode/ada/*.vsix .
+git checkout integration/vscode/ada/package.json
+rm -rf integration/vscode/ada/{linux,darwin,win32}
