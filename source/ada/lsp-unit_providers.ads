2b882965fa6315afdaa6eafd7e54defd70bbc524
Nicolas Setton <setton@gmail.com>
2021-03-18 11:42:19 +0000

    Cleanup code in LSP.Unit_Providers

--- source/ada/lsp-unit_providers.ads
+++ source/ada/lsp-unit_providers.ads
@@ -19,7 +19,7 @@
 --  from disk. This is copied from Libadalang, see "NOTE" in the body.
 
 with GNATCOLL.Projects;
-with GNATCOLL.Traces; use GNATCOLL.Traces;
+with GNATCOLL.Traces;   use GNATCOLL.Traces;
 
 with Libadalang.Analysis;
 with Libadalang.Common; use Libadalang.Common;
@@ -28,31 +28,20 @@ package LSP.Unit_Providers is
 
    package LAL renames Libadalang.Analysis;
    package Prj renames GNATCOLL.Projects;
+   use type GNATCOLL.Projects.Project_Type;
 
    Trace : constant GNATCOLL.Traces.Trace_Handle := GNATCOLL.Traces.Create
      ("LIBADALANG.PROJECT_PROVIDER", GNATCOLL.Traces.From_Config);
 
-   type Provider_And_Projects is record
-      Provider : LAL.Unit_Provider_Reference;
-      Projects : Prj.Project_Array_Access;
-   end record;
-   --  Associates one project unit provider with all the projects on which it
-   --  has visibility.
-
    function Create_Project_Unit_Provider
      (Tree             : Prj.Project_Tree_Access;
-      Project          : Prj.Project_Type := Prj.No_Project;
+      Project          : Prj.Project_Type;
       Env              : Prj.Project_Environment_Access;
       Is_Project_Owner : Boolean := True)
-      return LAL.Unit_Provider_Reference;
-   --  Likewise, but create only one unit provider.
-   --
-   --  If a non-null Project is given, use it to provide units. Raise an
-   --  Invalid_Project exception if an aggregate projects that aggregates more
-   --  than one project is in its closure.
-   --
-   --  If Project is not provided, run Create_Project_Unit_Providers: if it
-   --  returns only one provider, return it, otherwise raise an error.
+      return LAL.Unit_Provider_Reference
+      with Pre => Project /= Prj.No_Project
+        and not Project.Is_Aggregate_Project;
+   --  Create an Unit Provider for the given non-aggregate project.
    --
    --  If ``Is_Project_Owner`` is true, the result owns ``Tree``, thus the
    --  caller must not deallocate it itself.  Otherwise, the project pointed to
