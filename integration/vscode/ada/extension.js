66f7598c741446d8558d8377c5c216f42088b3e2
Maxim Reznik <reznik@adacore.com>
2021-03-31 11:51:09 +0300

    Add a task provider with "build project"/"check file"
    auto-detected tasks.
    
    Refs #541

--- integration/vscode/ada/extension.js
+++ integration/vscode/ada/extension.js
@@ -1,8 +1,27 @@
+/*----------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                     Copyright (C) 2018-2021, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+----------------------------------------------------------------------------*/
+
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
-const vscode_1 = require("vscode");
+const vscode = require("vscode");
 const vscode_languageclient_1 = require("vscode-languageclient");
 const process = require("process");
+const gprTaskProvider = require("./gprTaskProvider");
+let myTaskProvider;
 function activate(context) {
     // The server is implemented in node
     let serverModule = context.asAbsolutePath(process.platform + '/ada_language_server');
@@ -23,13 +42,25 @@ function activate(context) {
             // Synchronize the setting section 'ada' to the server
             configurationSection: 'ada',
             // Notify the server about file changes to '.clientrc files contain in the workspace
-            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+            fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
         }
     };
     // Create the language client and start the client.
-    let disposable = new vscode_languageclient_1.LanguageClient('ada', 'Ada Language Server', serverOptions, clientOptions).start();
+    let disposable = new vscode_languageclient_1.LanguageClient
+      ('ada', 'Ada Language Server', serverOptions, clientOptions).start();
     // Push the disposable to the context's subscriptions so that the 
     // client can be deactivated on extension deactivation
     context.subscriptions.push(disposable);
+    myTaskProvider = vscode.tasks.registerTaskProvider
+      (gprTaskProvider.GPRTaskProvider.gprBuildType,
+       new gprTaskProvider.GPRTaskProvider());
 }
 exports.activate = activate;
+
+function deactivate() {
+    if (myTaskProvider) {
+        myTaskProvider.dispose();
+        myTaskProvider = undefined;
+    }
+}
+exports.deactivate = deactivate;
