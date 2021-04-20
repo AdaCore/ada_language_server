f2bed5a5d247eeb7a57f5d84bc10da60e777cd46
Nicolas Setton <setton@adacore.com>
2021-04-20 11:45:16 +0100

    U415-035 Make use of the Libadalang File_Reader object
    This object is in charge of providing LAL with the contents of the
    file. The LSP implementation of the File_Reader supports two
    main features:
    
      - getting the contents from open Documents automatically
      - appling a simple-minded preprocessing on the fly
    
    This allows us to do the following:
    
      - remove all calls to Get_From_Buffer: this is no longer needed,
        the File_Reader takes care of this.
    
      - remove the need to call our own function "Get_From_File": since
        LAL calls the File_Reader systematically, we no longer need to
        intercept the call
    
      - remove our own Unit_Provider: it was used to provide preprocessing
        and is no longer needed.

---
+++ source/ada/lsp-ada_handlers-file_readers.adb
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                        Copyright (C) 2021, AdaCore                       --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with VSS.Strings;       use VSS.Strings;
+with LSP.Ada_Documents; use LSP.Ada_Documents;
+with LSP.Preprocessor;  use LSP.Preprocessor;
+
+package body LSP.Ada_Handlers.File_Readers is
+
+   ----------
+   -- Read --
+   ----------
+
+   overriding procedure Read
+     (Self        : LSP_Reader_Interface;
+      Filename    : String;
+      Charset     : String;
+      Read_BOM    : Boolean;
+      Contents    : out Langkit_Support.File_Readers.Decoded_File_Contents;
+      Diagnostics : in out
+        Langkit_Support.Diagnostics.Diagnostics_Vectors.Vector)
+   is
+      Doc : Document_Access;
+   begin
+      --  First check if the file is an open document
+      Doc := Self.Handler.Get_Open_Document
+        (URI   => Self.Handler.File_To_URI
+           (LSP.Types.To_LSP_String (Filename)),
+         Force => False);
+
+      if Doc /= null then
+         --  There is a document - we can get this and preprocess
+         Contents := Preprocess_Buffer (Buffer => Doc.Text);
+
+      else
+         --  No open document: preprocess from the file
+         Contents := Preprocess_File (Filename => Filename,
+                                      Charset  => Charset);
+      end if;
+   end Read;
+
+end LSP.Ada_Handlers.File_Readers;
