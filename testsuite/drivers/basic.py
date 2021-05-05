9904d58e27bfa9050cb52fd7a277dcc32b004357
Nicolas Setton <setton@adacore.com>
2021-05-05 10:54:19 +0100

    T608-018 Deactivate timeout in the driver
    e3-testsuite-driver relies on rlimit to implement timeouts,
    and this does not seem to work on GitHub Action machines.
    
    Deactivate this timeout, and rely instead on the fine-grained
    timeout mechanism implemented by the test runner.

--- testsuite/drivers/basic.py
+++ testsuite/drivers/basic.py
@@ -20,21 +20,23 @@ class JsonTestDriver(ALSTestDriver):
             return False
 
         # The working directory
-        wd = self.test_env['working_dir']
+        wd = self.test_env["working_dir"]
 
         output = ""
 
         status = TestStatus.PASS
 
-        for json in glob.glob(os.path.join(wd, '*.json')):
+        for json in glob.glob(os.path.join(wd, "*.json")):
             process = self.run_and_log(
                 [self.env.tester_run, json],
                 cwd=wd,
-                timeout=min(780, 120 * self.env.wait_factor),
-                env={'ALS': self.env.als,
-                     'ALS_HOME': self.env.als_home,
-                     'ALS_WAIT_FACTOR': str(self.env.wait_factor)},
-                ignore_environ=False)
+                env={
+                    "ALS": self.env.als,
+                    "ALS_HOME": self.env.als_home,
+                    "ALS_WAIT_FACTOR": str(self.env.wait_factor),
+                },
+                ignore_environ=False,
+            )
             output += process.out
 
             if process.status:
