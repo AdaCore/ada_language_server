a4b4f32071e58fbd6a57c36a46c4e0a7878170ce
Maxim Reznik <reznik@adacore.com>
2021-07-05 16:44:06 +0300

    U622-019 Move keywoard completion code
    into a dedicated compilation unit.

---
+++ source/ada/lsp-ada_completions-keywords.adb
@@ -0,0 +1,92 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                     Copyright (C) 2018-2021, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Strings.UTF_Encoding;
+
+with Langkit_Support.Text;
+with Libadalang.Common;
+
+with VSS.Strings;
+
+with LSP.Lal_Utils;
+with LSP.Types;
+
+package body LSP.Ada_Completions.Keywords is
+
+   ------------------------
+   -- Propose_Completion --
+   ------------------------
+
+   overriding procedure Propose_Completion
+     (Self   :     Keyword_Completion_Provider;
+      Sloc   :     Langkit_Support.Slocs.Source_Location;
+      Token  :     Libadalang.Common.Token_Reference;
+      Node   :     Libadalang.Analysis.Ada_Node;
+      Names  : out Ada_Completions.Completion_Maps.Map;
+      Result : out LSP.Messages.CompletionList)
+   is
+      pragma Unreferenced (Names);
+      Prev   : constant Libadalang.Common.Token_Reference :=
+        Libadalang.Common.Previous (Token);
+
+      Parent : constant Libadalang.Analysis.Ada_Node :=
+        (if Node.Is_Null then Node else Node.Parent);
+
+      In_End_Label : constant Boolean :=
+        Parent.Kind in Libadalang.Common.Ada_End_Name_Range;
+
+   begin
+      if In_End_Label or else not Libadalang.Common.Is_Trivia (Prev) then
+         --  Propose keyword completion if we are not within an end label
+         --  and if there is no previous character of if it's a whitespace (we
+         --  don't want to propose keywords after typing '(' to feed subprogram
+         --  parameters for instance).
+         return;
+      end if;
+
+      declare
+         Item   : LSP.Messages.CompletionItem;
+         Prefix : constant Ada.Strings.UTF_Encoding.UTF_8_String :=
+           Langkit_Support.Text.To_UTF8 (Node.Text);
+
+         Keywords : constant Libadalang.Analysis.Unbounded_Text_Type_Array :=
+           Node.P_Valid_Keywords;
+      begin
+         for Keyword of Keywords loop
+            declare
+               Label : constant VSS.Strings.Virtual_String :=
+                 LSP.Lal_Utils.To_Virtual_String (Keyword);
+               String : constant LSP.Types.LSP_String :=
+                 LSP.Types.To_LSP_String (Label);
+            begin
+               if LSP.Types.Starts_With
+                 (Text           => String,
+                  Prefix         => Prefix,
+                  Case_Sensitive => False)
+               then
+                  Item.label := Label;
+                  Item.insertTextFormat := (True, LSP.Messages.PlainText);
+                  Item.insertText := (True, String);
+                  Item.kind := (True, LSP.Messages.Keyword);
+                  Result.items.Append (Item);
+               end if;
+            end;
+         end loop;
+      end;
+   end Propose_Completion;
+
+end LSP.Ada_Completions.Keywords;
