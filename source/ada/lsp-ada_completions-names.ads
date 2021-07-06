be0166bef7dcafaf98abf2dcc844e312670d2d33
Maxim Reznik <reznik@adacore.com>
2021-07-06 10:03:06 +0300

    U622-019 Move name completion code
    into a dedicated compilation unit

---
+++ source/ada/lsp-ada_completions-names.ads
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------------
+--                         Language Server Protocol                         --
+--                                                                          --
+--                     Copyright (C) 2018-2021, AdaCore                     --
+--                                                                          --
+-- This is free software;  you can redistribute it  and/or modify it  under --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+--  A completion provider for names (corresponding to some defining names).
+
+package LSP.Ada_Completions.Names is
+
+   type Name_Completion_Provider (Snippets_Enabled : Boolean)
+     is new Completion_Provider with null record;
+
+   overriding procedure Propose_Completion
+     (Self   : Name_Completion_Provider;
+      Sloc   : Langkit_Support.Slocs.Source_Location;
+      Token  : Libadalang.Common.Token_Reference;
+      Node   : Libadalang.Analysis.Ada_Node;
+      Names  : out Ada_Completions.Completion_Maps.Map;
+      Result : out LSP.Messages.CompletionList);
+
+end LSP.Ada_Completions.Names;
