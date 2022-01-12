ba4379b92cb6d87727981f395994c6e4012d203f
Joao Azevedo <azevedo@adacore.com>
2022-01-12 13:16:40 +0000

    UC17-023 Fix syntax error

--- testsuite/ada_lsp/UA28-007.Did_Change_Watched_Files_0/replace.py
+++ testsuite/ada_lsp/UA28-007.Did_Change_Watched_Files_0/replace.py
@@ -5,16 +5,16 @@ import os
 
 # Arguments: 1) Old Name 2) New Name 3) Target File
 if len(sys.argv) != 4:
-    raise Program_Error ("Invalid arguments")
+    raise ValueError("Invalid arguments")
 
-file_path=os.path.dirname(sys.argv[3])
+file_path = os.path.dirname(sys.argv[3])
 if not os.path.exists(file_path):
-    raise Program_Error (f"Invalid file path: $filepath")
+    raise ValueError(f"Invalid file path: {file_path}")
 
-with open(sys.argv[3], 'r') as file :
-  filedata = file.read()
+with open(sys.argv[3], "r") as file:
+    filedata = file.read()
 
 filedata = filedata.replace(sys.argv[1], sys.argv[2])
 
-with open(sys.argv[3], 'w') as file:
-  file.write(filedata)
+with open(sys.argv[3], "w") as file:
+    file.write(filedata)
