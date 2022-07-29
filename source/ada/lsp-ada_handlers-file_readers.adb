c55130e1e1a064024a93281e940823e7848aa828
Anthony Leonardo Gracio <leonardo@adacore.com>
2022-07-29 11:52:23 +0200

    V315-025: Use new LAL's API for preprocessing
    Instead of our custom one.

--- source/ada/lsp-ada_handlers-file_readers.adb
+++ source/ada/lsp-ada_handlers-file_readers.adb
@@ -15,11 +15,17 @@
 -- of the license.                                                          --
 ------------------------------------------------------------------------------
 
-with VSS.Strings;       use VSS.Strings;
-with LSP.Ada_Documents; use LSP.Ada_Documents;
-with LSP.Preprocessor;  use LSP.Preprocessor;
+with GNAT.Strings;                 use GNAT.Strings;
+with GNATCOLL.Traces;              use GNATCOLL.Traces;
+with VSS.Strings;                  use VSS.Strings;
+with VSS.Strings.Conversions;
+with LSP.Ada_Documents;            use LSP.Ada_Documents;
+with Libadalang.Preprocessing;     use Libadalang.Preprocessing;
+with Langkit_Support.File_Readers; use Langkit_Support.File_Readers;
 
 package body LSP.Ada_Handlers.File_Readers is
+
+   Me : constant Trace_Handle := Create ("ALS.FILE_READERS");
 
    ----------
    -- Read --
@@ -34,22 +40,65 @@ package body LSP.Ada_Handlers.File_Readers is
       Diagnostics : in out
         Langkit_Support.Diagnostics.Diagnostics_Vectors.Vector)
    is
-      Doc : Document_Access;
+      Doc    : Document_Access;
+      Source : Preprocessed_Source := Preprocessed_Source'
+        (Buffer => null, Last => 0);
+      Buffer : GNAT.Strings.String_Access;
    begin
       --  First check if the file is an open document
       Doc := Self.Handler.Get_Open_Document
         (URI   => LSP.Types.File_To_URI (Filename),
          Force => False);
 
+      --  Preprocess the document's contents if open, or the file contents if
+      --  not.
       if Doc /= null then
-         --  There is a document - we can get this and preprocess
-         Contents := Preprocess_Buffer (Buffer => Doc.Text);
-
+         Buffer := new String'
+           (VSS.Strings.Conversions.To_UTF_8_String (Doc.Text));
       else
-         --  No open document: preprocess from the file
-         Contents := Preprocess_File (Filename => Filename,
-                                      Charset  => Charset);
+         Buffer := Create_From_UTF8 (Filename).Read_File;
+
+         --  Return an empty sring when failing to read the file (i.e: when the
+         --  file has been deleted).
+         if Buffer = null then
+            Buffer := new String'("");
+         end if;
       end if;
+
+      --  If we have preprocessing data, use LAL's API to preoprocess the file.
+      --  Otherwise, just decode the contents of the document/file.
+
+      if Self.Preprocessing_Data /= No_Preprocessor_Data then
+         Libadalang.Preprocessing.Preprocess
+           (Data        => Self.Preprocessing_Data,
+            Filename    => Filename,
+            Input       => Buffer.all,
+            Contents    => Source,
+            Diagnostics => Diagnostics);
+
+         if Source.Buffer = null then
+            --  Log the diagnostics when processing has failed
+            for Diag of Diagnostics loop
+               Me.Trace (Langkit_Support.Diagnostics.To_Pretty_String (Diag));
+            end loop;
+         end if;
+      end if;
+
+      --  Decode the preprocessed buffer (or the initial contents when there is
+      --  no preprocessing needed) in utf-8.
+
+      Decode_Buffer
+        (Buffer      => (if Source.Buffer /= null then
+                            Source.Buffer (1 .. Source.Last)
+                         else
+                            Buffer.all),
+         Charset     => "utf-8",
+         Read_BOM    => Read_BOM,
+         Contents    => Contents,
+         Diagnostics => Diagnostics);
+
+      Free (Source);
+      GNAT.Strings.Free (Buffer);
    end Read;
 
 end LSP.Ada_Handlers.File_Readers;
