fa88408a37aee6a41a39d86064883ba4bf3f6ac9
Maxim Reznik <reznikmm@gmail.com>
2022-11-07 14:58:43 +0200

    Fix semanticTokens request
    to avoid returning token types/modifiers not supported by the client
    and avoid raising CONSTRAINT_ERROR.
    
    Fixes #1070

---
+++ testsuite/ada_lsp/V628-012.highlighting.no-libs/foo.adb
@@ -0,0 +1,9 @@
+with Ada.Unchecked_Conversion;
+procedure Foo is
+
+   type My_Type is new Integer range 1 .. 10;
+
+   X, Y : My_Type := 1;  --  Decl with multiple defining names
+begin
+   X := Y + My_Type (9);
+end Foo;
