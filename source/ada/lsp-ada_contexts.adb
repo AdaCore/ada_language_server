9d1cc544abac06b4976773114d89e83bd3cb03d7
Anthony Leonardo Gracio <leonardo@adacore.com>
2023-01-25 12:25:30 +0100

    Revert "UC15-036: Include externally built source files"
    This reverts commit 3d920299b2a20fed48e6e999d5e44ddeeab79de4.

--- source/ada/lsp-ada_contexts.adb
+++ source/ada/lsp-ada_contexts.adb
@@ -725,7 +725,7 @@ package body LSP.Ada_Contexts is
            (View             => Root,
             Action           => Insert_Source'Access,
             Language         => GPR2.Ada_Language,
-            Externally_Built => True);
+            Externally_Built => False);
 
          Self.Source_Dirs.Clear;
 
