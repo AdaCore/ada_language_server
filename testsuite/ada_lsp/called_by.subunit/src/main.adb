9114cbea6046ae1180008571d96261f21dfb83ad
Boulanger <Boulanger Adrien>
2023-10-30 11:57:15 +0100

    Manual cherry_pick of Improve prepareCallHierarchy
    See: eng/ide/ada_language_server!1410
    eng/ide/ada_language_server#1193

---
+++ testsuite/ada_lsp/called_by.subunit/src/main.adb
@@ -0,0 +1,7 @@
+with Root.Child;
+with Root.Child.Do_Something;
+procedure Main is
+begin
+   Root.Child.Do_Something;
+   Root.Child.Hello;
+end Main;
---
+++ testsuite/ada_lsp/called_by.subunit/src/root-child-do_something.adb
@@ -0,0 +1,4 @@
+procedure Root.Child.Do_Something is
+begin
+   null;
+end Root.Child.Do_Something;
