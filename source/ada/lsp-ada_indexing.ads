90cf413b3493ec9e4cce3f3bc3a1ae2925e53971
Maxim Reznik <reznikmm@gmail.com>
2024-06-17 17:24:31 +0300

    Add Lowest job priority
    for the indexing job. Change job scheduler to keep indexing jobs
    while jobs with Fence priority are executing.
    
    Fixes #1362

--- source/ada/lsp-ada_indexing.ads
+++ source/ada/lsp-ada_indexing.ads
@@ -86,7 +86,7 @@ private
 
    overriding function Priority
      (Self : Indexing_Job) return LSP.Server_Jobs.Job_Priority is
-       (LSP.Server_Jobs.Low);
+       (LSP.Server_Jobs.Lowest);
 
    overriding procedure Execute
      (Self   : in out Indexing_Job;
