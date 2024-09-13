5fcc98c6b47508f563bbfe911b0f42176515f2b0
Anthony Leonardo Gracio <leonardo@adacore.com>
2024-09-13 14:51:49 +0000

    Update ESLint config file

---
+++ integration/vscode/ada/eslint.config.mjs
@@ -0,0 +1,61 @@
+import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
+import tsdoc from "eslint-plugin-tsdoc";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: ["**/out", "**/*.d.ts"],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslintEslintPlugin,
+        tsdoc,
+    },
+
+    languageOptions: {
+        globals: {
+            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+            ...globals.node,
+            ...globals.commonjs,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "commonjs",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+
+            project: "tsconfig.json",
+        },
+    },
+
+    rules: {
+        "tsdoc/syntax": "warn",
+
+        "max-len": ["warn", {
+            code: 100,
+            ignoreUrls: true,
+        }],
+
+        "@typescript-eslint/switch-exhaustiveness-check": "error",
+    },
+}];
