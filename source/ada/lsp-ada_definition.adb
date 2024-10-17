745e0eeacfc772c35905f034dde5b3351fbc987a
Boulanger <Boulanger Adrien>
2024-10-17 16:29:47 +0200

    Fix Goto Definition for entry in Protected Types
    Add a test.
    
    Closes eng/ide/ada_language_server#1437

--- source/ada/lsp-ada_definition.adb
+++ source/ada/lsp-ada_definition.adb
@@ -122,7 +122,6 @@ package body LSP.Ada_Definition is
       Manual_Fallback         : Libadalang.Analysis.Defining_Name;
       Definition_Node         : Libadalang.Analysis.Basic_Decl;
       Decl_For_Find_Overrides : Libadalang.Analysis.Basic_Decl;
-      Entry_Decl_Node         : Libadalang.Analysis.Entry_Decl;
 
       Ignore : Boolean;
    begin
@@ -186,7 +185,37 @@ package body LSP.Ada_Definition is
 
          --  Search for accept statements only if we are on an entry
          if Definition_Node.Kind in Libadalang.Common.Ada_Entry_Decl_Range then
-            Entry_Decl_Node := Definition_Node.As_Entry_Decl;
+            declare
+               Entry_Decl_Node   : constant Libadalang.Analysis.Entry_Decl :=
+                 Definition_Node.As_Entry_Decl;
+               Entry_Parent_Node : constant Libadalang.Analysis.Basic_Decl :=
+                 Entry_Decl_Node.P_Parent_Basic_Decl;
+            begin
+               --  P_Accept_Stmts is only valid for entries declared in tasks
+               if Entry_Parent_Node.Kind in
+                 Libadalang.Common.Ada_Task_Type_Decl_Range
+               then
+                  for Accept_Node of Entry_Decl_Node.P_Accept_Stmts loop
+                     Self.Parent.Context.Append_Location
+                       (Self.Response,
+                        Self.Filter,
+                        Accept_Node.F_Body_Decl.F_Name);
+                  end loop;
+
+               --  Others entries are are handled as simple subprograms
+               else
+                  declare
+                     Other_Part_For_Decl : constant
+                       Libadalang.Analysis.Basic_Decl :=
+                       Laltools.Common.Find_Next_Part_For_Decl
+                         (Definition_Node, Trace);
+                  begin
+                     if not Other_Part_For_Decl.Is_Null then
+                        Other_Part := Other_Part_For_Decl.P_Defining_Name;
+                     end if;
+                  end;
+               end if;
+            end;
 
          elsif Definition_Node.Kind in
            Libadalang.Common.Ada_Single_Task_Type_Decl_Range |
@@ -264,15 +293,6 @@ package body LSP.Ada_Definition is
                   Is_Child);
             end loop;
          end;
-      end if;
-
-      if not Entry_Decl_Node.Is_Null then
-         for Accept_Node of Entry_Decl_Node.P_Accept_Stmts loop
-            Self.Parent.Context.Append_Location
-              (Self.Response,
-               Self.Filter,
-               Accept_Node.F_Body_Decl.F_Name);
-         end loop;
       end if;
    end Execute_Ada_Request;
 
